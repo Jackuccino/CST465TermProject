b9d655d9c0da290a9268299a391f2f3a39eb7a8d
JinJie Xu <jinjie.xu@oit.edu>
2018-10-16 23:05:06 -0700

    Added the new Assignment2 project

---
+++ Assignment2/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person model)
+        {
+            return View("PersonInfo", model);
+        }
+    }
+}
